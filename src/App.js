f874940b1c46786c5c677d66f9b9881808880197
Sean Febo <sfeebs@Seans-MacBook-Pro-2.local>
2018-08-17 14:03:26 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
-// create your App component here
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+
+	constructor() { 
+		super()
+	}
+
+	render() { 
+	}
+
+	componentDidMount() { 
+		fetch('http://api.open-notify.org/astros.json')
+	}
+}
+
+export default App 
