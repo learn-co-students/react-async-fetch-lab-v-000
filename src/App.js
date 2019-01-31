5867783fa1940b9098934bfa1630ece1fc94d821
Brian <bbirdwell@gmail.com>
2019-01-31 09:43:00 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
 
 export default class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			astros: ''
-		}
+	state = {
+			astros: []
 	}
 
 	componentDidMount() {
 		fetch('http://api.open-notify.org/astros.json')
-		.then(data => this.setState({
-			astros: data
-		}))
+		.then(data => data.json())
+		// .then(astros => console.log(astros))
+		.then(data => this.setState({ astros: data.people }))
+
 	}
 
 	render() {
-
+		return (
+			<div>
+				{this.state.astros.map((astro, index) =>
+					<li key={index}>{astro.name}</li>
+				)}
+			</div>
+		)
 	}
 }
