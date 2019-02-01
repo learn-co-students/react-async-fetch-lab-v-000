f9f5368fafc3891610fded299621b8d6f670b035
Dalma Boros <dalma.boros@gmail.com>
2019-02-01 09:40:39 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      "something"
+    )
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        peopleInSpace: data.people
+      })
+    })
+  }
+}
+
+export default App
