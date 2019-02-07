0ce84e9876ee9b41cf2be5beaba7b47320745b4f
Vibraniumforge <gregm9999@gmail.com>
2019-02-06 20:41:39 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React, {Component} from 'react'
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            astronauts: []
+        }
+    }
+   
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json())
+        .then(astronauts => this.setState({astronauts}))
+        .catch(err=> console.log("Error"))
+    }
+
+    render() {
+        const astronauts = this.state.astronauts.map((astro, index) => (
+            <p>astronaut={astro} key={index}</p>
+        ))
+        return (
+            <div>
+                {astronauts}
+            </div>
+        )
+    }
+}
+
+export default App;
