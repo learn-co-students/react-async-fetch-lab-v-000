1b9a0034a1f46ff0361dd263b8fa11a115396e07
Madeline-Stark <madeline.stark1@gmail.com>
2019-09-21 16:25:37 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import React, {Component} from 'react'
 
 export default class App extends Component {
 
-  componentDidMount(){
-    fetch('http://api.open-notify.org/astros.json')
-      .then(response => (response.json()))
-      .then(data => data)
-
+  constructor(){
+    super()
+    this.state = {
+      astronauts: [] //starts as empty array
+    }
   }
+
+  listAstronauts = () => {
+    return this.state.astronauts.map((astro, id) => {
+      return <li key={id}>{astro.name}</li>
+    })
+  }
+
   render(){
     return (
-      'boop'
+      <div>
+       {this.listAstronauts()}
+     </div>
     )
   }
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          astronauts: data.people //was getting error b/c just doing data
+        })
+      })
+    }
 }
