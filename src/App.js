46c3068679ca2134fe2edbb59af704637e14bbdb
github username <github email address>
2019-10-19 17:10:26 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-
+  state = {
+       peopleInSpace: []
+   }
 
 
   render(){
-
+    return (
+          <div>
+            {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+          </div>
+        )
   }
 
   componentDidMount() {
