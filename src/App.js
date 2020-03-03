4006adb23c888099492f8dc6f8c4c3db3ca538ba
Thomas Gray <thomas.gray@tgray.fios-router.home>
2020-03-03 09:08:40 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp => resp.json())
+    .then(astronauts => this.setState({
+      astronauts: astronauts.people.map(astronaut => astronaut.name)
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.astronauts}
+      </div>
+    )
+  }
+}
