a11bb53491303cd64ef10648453f478e4b16ac63
William Barela <william.barela.dev@gmail.com>
2020-08-22 18:37:07 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+
+import React from 'react';
+
+// Achtung! this is an antipattern, but the API does not have an id, so this has to be done
+function ListItem(props) {
+  return(
+    <li key={props.id}>{props.name}</li>
+  )
+}
+
+export default class App extends React.Component {
+  state = {
+    peopleInSpace: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
+  }
+
+  render() {
+    return(
+      <ul>
+        {this.state.peopleInSpace.map((person,id) => <ListItem name={person.name} key={id} />)}
+      </ul>
+    )
+  }
+}
