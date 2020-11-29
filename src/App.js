235f593136f8d344ffe71d7365b4830959ddf1b1
KimMead <kimmeadows8@gmail.com>
2020-11-29 15:37:44 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,47 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loaded: false,
+            data: []
+        }
+    }
+
+componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json').then(r=>r.json()).then(data => {
+        this.setState({
+            loaded: true,
+            data: data
+        })
+    })
+}
+
+render() {
+    if (this.state.loaded) {
+        const mappedAstronauts = this.state.data.people.map(person => {
+            return(
+                <div>
+                    <p>{person.name}</p>
+                    <p>{person.craft}</p>
+                </div>
+            )
+        })
+
+    return(
+        <div>{mappedAstronauts}
+        </div>
+    )
+    }
+
+    else {
+        return(
+            <h1>Loading</h1>
+        )
+    }
+}
+}
+
+export default App
